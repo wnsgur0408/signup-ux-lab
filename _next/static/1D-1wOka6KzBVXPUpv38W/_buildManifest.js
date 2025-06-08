b3bb9171c52ba0858475a38acae5091594c7fd05
wnsgur0408 <wnsgurah2009@gmail.com>
2025-06-08 22:23:14 +0900

    Updates

--- _next/static/1D-1wOka6KzBVXPUpv38W/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-49358202f86af1e4.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
